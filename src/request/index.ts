282f4480fabf1c75801e579a7777a53e15884b16
Aine <Tsos201@outlook.com>
2022-08-17 14:59:02 +0800

    修改项目结构，新增部分方法

---
+++ src/request/index.ts
@@ -0,0 +1,19 @@
+// 此文件记录所有请求
+
+const baseurl = "http://127.0.0.1:8886/insipid/";
+
+// 后缀路径列表
+const RequestSoftware = {
+    // 请求所有软件列表（相当于请求所有软件的状态）
+    requestSoftwareList: "request/software/list",
+    // 请求指定软件的状态
+    requestSoftwareStatus: "request/software/status",
+    // 请求指定软件的下载地址
+    requestSoftwareDownload: "request/software/download",
+    // 请求指定软件的安装脚本
+    requestScriptInstall: "request/script/install",
+    // 请求指定软件的升级脚本
+    requestScriptUpdate: "request/script/update",
+    // 请求指定软件的卸载脚本
+    requestScriptUninstall: "request/script/uninstall",
+}
