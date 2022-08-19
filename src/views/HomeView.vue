cc1986d568f512df93378e66d0ddd982a079e4e1
Aine <Tsos201@outlook.com>
2022-08-19 17:41:32 +0800

    页面修改

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
-    <h1>Insipid Startup</h1>
-    <h5>Version 0.1.0 内部测试版本</h5>
-    
+    <h1>Insipid</h1>
+    <h5>精简/开源/自由的软件/脚本控制器</h5>
+
   </div>
 </template>
 
