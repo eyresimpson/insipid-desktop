46113af8d7182226da465601aa83e48eec75cb95
Aine <Tsos201@outlook.com>
2022-08-25 10:00:26 +0800

    页面组件拆分

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,59 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import ManageView from '../views/ManageView.vue'
-import StoreView from '../views/StoreView.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import ManageView from "../views/ManageView.vue";
+import StoreView from "../views/StoreView.vue";
+import systemConfigComponent from "../components/configure/systemConfigComponent.vue";
+import networkConfigComponent from "../components/configure/networkConfigComponent.vue";
+import securityConfigComponent from "../components/configure/securityConfigComponent.vue";
+import otherConfigComponent from "../components/configure/otherConfigComponent.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/store',
-    name: 'store',
-    component: StoreView
+    path: "/store",
+    name: "store",
+    component: StoreView,
   },
   {
-    path: '/manage',
-    name: 'manage',
-    component: ManageView
+    path: "/manage",
+    name: "manage",
+    component: ManageView,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: AboutView
-  }
-]
+    path: "/about",
+    name: "about",
+    component: AboutView,
+  },
+  {
+    path: "/config/sys",
+    name: "system",
+    component: systemConfigComponent,
+  },
+  {
+    path: "/config/net",
+    name: "network",
+    component: networkConfigComponent,
+  },
+  {
+    path: "/config/sec",
+    name: "security",
+    component: securityConfigComponent,
+  },
+  {
+    path: "/config/oth",
+    name: "other",
+    component: otherConfigComponent,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -1,41 +1,63 @@
 <template>
   <div>
-    <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="true" @open="handleOpen" @close="handleClose">
-      <el-menu-item index="1">
-        <el-icon>
-          <Operation />
-        </el-icon>
-        <template #title>系统</template>
-      </el-menu-item>
-      <el-menu-item index="2">
-        <el-icon>
-          <Connection />
-        </el-icon>
-        <template #title>网络</template>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <el-icon>
-          <Key />
-        </el-icon>
-        <template #title>安全</template>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <el-icon>
-          <setting />
-        </el-icon>
-        <template #title>其他</template>
-      </el-menu-item>
-    </el-menu>
+    <el-row>
+      <el-col :span="2">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          :collapse="true"
+          @open="handleOpen"
+          @close="handleClose"
+          :router="true"
+        >
+          <el-menu-item index="1">
+            <el-icon>
+              <Operation />
+            </el-icon>
+            <template #title>系统</template>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <el-icon>
+              <Connection />
+            </el-icon>
+            <template #title>网络</template>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <el-icon>
+              <Key />
+            </el-icon>
+            <template #title>安全</template>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon>
+              <setting />
+            </el-icon>
+            <template #title>其他</template>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :span="22">
+        <div class="content">
+          <router-view />
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
 
 const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
+  console.log(key, keyPath);
+};
 const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-
+  console.log(key, keyPath);
+};
 </script>
+<style>
+.content {
+  background-color: azure;
+  height: 100%;
+}
+</style>
