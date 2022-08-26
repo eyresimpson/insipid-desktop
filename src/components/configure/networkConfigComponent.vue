a0105a23707ed8baa4d865fb74a8fa94838733e9
Aine <Tsos201@outlook.com>
2022-08-26 10:11:30 +0800

    修改配置页面

--- src/components/configure/networkConfigComponent.vue
+++ src/components/configure/networkConfigComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>关于</h1>
+    <h1>网络</h1>
     <h5>遵循MIT协议，完全开源</h5>
   </div>
 </template>
--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -6,28 +6,27 @@
           default-active="2"
           class="el-menu-vertical-demo"
           :collapse="true"
-          @open="handleOpen"
-          @close="handleClose"
+          @select="handleSelect"
         >
-          <el-menu-item index="1">
+          <el-menu-item index="system">
             <el-icon>
               <Operation />
             </el-icon>
             <template #title>系统</template>
           </el-menu-item>
-          <el-menu-item index="2">
+          <el-menu-item index="network">
             <el-icon>
               <Connection />
             </el-icon>
             <template #title>网络</template>
           </el-menu-item>
-          <el-menu-item index="3">
+          <el-menu-item index="security">
             <el-icon>
               <Key />
             </el-icon>
             <template #title>安全</template>
           </el-menu-item>
-          <el-menu-item index="4">
+          <el-menu-item index="other">
             <el-icon>
               <setting />
             </el-icon>
@@ -37,7 +36,11 @@
       </el-col>
       <el-col :span="22">
         <div class="content">
-          <router-view />
+          <!-- <router-view /> -->
+          <systemConfigComponent :v-if="selected=='system'"></systemConfigComponent>
+          <networkConfigComponent :v-if="selected=='network'"></networkConfigComponent>
+          <securityConfigComponent :v-if="selected=='security'"></securityConfigComponent>
+          <otherConfigComponent :v-if="selected=='other'"></otherConfigComponent>
         </div>
       </el-col>
     </el-row>
@@ -45,13 +48,29 @@
 </template>
 
 <script lang="ts" setup>
+import systemConfigComponent from "../components/configure/systemConfigComponent.vue";
+import networkConfigComponent from "../components/configure/networkConfigComponent.vue";
+import securityConfigComponent from "../components/configure/securityConfigComponent.vue";
+import otherConfigComponent from "../components/configure/otherConfigComponent.vue";
 import { ref } from "vue";
 
-const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
-};
-const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
+const selected = ref("system")
+
+const handleSelect = (key: string) => {
+  switch (key) {
+    case "system":
+      selected.value = "system";
+      break;
+    case "network":
+      selected.value = "network";
+      break;
+    case "security":
+      selected.value = "security";
+      break;
+    case "other":
+      selected.value = "other";
+      break;
+  }
 };
 </script>
 <style>
