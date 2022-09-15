d7a4c377e2eda357b964516e4e3c0f9f4b1b8420
Aine <Tsos201@outlook.com>
2022-09-15 09:24:35 +0800

    样式修改

--- src/components/store/StandardItemFlow.vue
+++ src/components/store/StandardItemFlow.vue
@@ -2,17 +2,26 @@
   <div class="item">
     <!-- 软件卡片 -->
     <el-row>
+      <el-col>
+        <el-image src="favicon.ico" fit="fill"></el-image>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <div class="title">{{ (app as Application).name }}</div>
+      </el-col>
+    </el-row>
+    <!-- <el-row>
       <el-col :span="6">
         <div class="title">{{ (app as Application).name }}</div>
       </el-col>
       <el-col class="description" :span="12">{{
         (app as Application).description
       }}</el-col>
-      
       <el-col :span="4">
         <el-button type="default">{{(app as Application).state == "uninstalled" ? "安装" : "管理"}}</el-button>
       </el-col>
-    </el-row>
+    </el-row> -->
   </div>
 </template>
 
@@ -27,13 +36,15 @@ defineProps({
 <style>
 .item {
   background: rgb(247, 247, 247);
-  border: 1px solid rgb(214, 214, 214);
+  /* border: 1px solid rgb(214, 214, 214); */
   padding-top: 25px;
   padding-bottom: 25px;
   margin: 5px;
+  width: 20%;
 }
 .title {
   color: rgb(77, 77, 77);
+  padding-top: 15px;
 }
 .description {
   text-align: left;
